63696849d3b914ffb57e3f2e5d9d9a10dd2b2bfa
Rowan Walker <rowanjkw@gmail.com>
2017-03-05 22:59:58 +1300

    no message

--- infra/main.tf
+++ infra/main.tf
@@ -2,36 +2,36 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
-  name = "MyDemoAPI4"
-  description = "This is my API for demonstration purposes"
+resource "aws_api_gateway_rest_api" "nutrition_information_api" {
+  name = "Nutrition.Information.Api"
+  description = "This is an API to retrieve nutritional information about a given item of food."
 }
 
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyDemoAPI.id}"
-  parent_id = "${aws_api_gateway_rest_api.MyDemoAPI.root_resource_id}"
-  path_part = "mydemoresource"
+resource "aws_api_gateway_resource" "nutrition_information_api_v1" {
+  rest_api_id = "${aws_api_gateway_rest_api.nutrition_information_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.nutrition_information_api.root_resource_id}"
+  path_part = "v1"
 }
 
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyDemoAPI.id}"
-  resource_id = "${aws_api_gateway_resource.MyDemoResource.id}"
+resource "aws_api_gateway_resource" "nutrition_information_api_v1_ping" {
+  rest_api_id = "${aws_api_gateway_rest_api.nutrition_information_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.nutrition_information_api_v1.root_resource_id}"
+  path_part = "ping"
+}
+
+resource "aws_api_gateway_method" "nutrition_information_api_v1_ping_method" {
+  rest_api_id = "${aws_api_gateway_rest_api.nutrition_information_api.id}"
+  resource_id = "${aws_api_gateway_resource.nutrition_information_api_v1_ping.id}"
   http_method = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "MyDemoIntegration" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyDemoAPI.id}"
-  resource_id = "${aws_api_gateway_resource.MyDemoResource.id}"
-  http_method = "${aws_api_gateway_method.MyDemoMethod.http_method}"
+resource "aws_api_gateway_integration" "nutrition_information_api_v1_ping_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.nutrition_information_api.id}"
+  resource_id = "${aws_api_gateway_resource.nutrition_information_api_v1_ping.id}"
+  http_method = "${aws_api_gateway_method.nutrition_information_api_v1_ping_method.http_method}"
   type = "MOCK"
-
-  # Transforms the incoming XML request to JSON
   request_templates {
-    "application/xml" = <<EOF
-    {
-    "body" : $input.json('$')
-    }
-    EOF
+    "statusCode": 200
   }
 }
