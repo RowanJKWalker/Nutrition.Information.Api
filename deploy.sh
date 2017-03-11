319da0b89a0c1a9050162ea22831927f4ff1d3b6
Rowan Walker <rowanjkw@gmail.com>
2017-03-11 22:42:23 +1300

    change to deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,11 @@ echo "after cd"
 terraform plan
 
 terraform apply
+
+terraform output nutrition_information_api_uri
+
+export NUTRITION_INFORMATION_API_URI=$(terraform output nutrition_information_api_uri) 
+
+npm run-script test:integration
+
+echo \"yes\" | terraform destroy
